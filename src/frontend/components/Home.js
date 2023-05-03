a93201cf6c04f5626b504623fd21c268a3a919b9
ntykwok <85721398+ntykwok@users.noreply.github.com>
2023-05-03 20:19:48 +0800

    setting up for frontend design

--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -49,38 +49,38 @@ const Home = ({ marketplace, nft }) => {
     </main>
   )
   return (
-    <div className="flex justify-center">
-      {items.length > 0 ?
-        <div className="px-5 container">
-          <Row xs={1} md={2} lg={4} className="g-4 py-5">
-            {items.map((item, idx) => (
-              <Col key={idx} className="overflow-hidden">
-                <Card>
-                  <Card.Img variant="top" src={item.image} />
-                  <Card.Body color="secondary">
-                    <Card.Title>{item.name}</Card.Title>
-                    <Card.Text>
-                      {item.description}
-                    </Card.Text>
-                  </Card.Body>
-                  <Card.Footer>
-                    <div className='d-grid'>
-                      <Button onClick={() => buyMarketItem(item)} variant="primary" size="lg">
-                        Buy for {ethers.utils.formatEther(item.totalPrice)} ETH
-                      </Button>
-                    </div>
-                  </Card.Footer>
-                </Card>
-              </Col>
-            ))}
-          </Row>
+      <div className="flex justify-center">
+        {items.length > 0 ?
+          <div className="px-5 container">
+            <Row xs={1} md={2} lg={4} className="g-4 py-5">
+              {items.map((item, idx) => (
+                <Col key={idx} className="overflow-hidden">
+                  <Card>
+                    <Card.Img variant="top" src={item.image} />
+                    <Card.Body color="secondary">
+                      <Card.Title>{item.name}</Card.Title>
+                      <Card.Text>
+                        {item.description}
+                      </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                      <div className='d-grid'>
+                        <Button onClick={() => buyMarketItem(item)} variant="primary" size="lg">
+                          Buy for {ethers.utils.formatEther(item.totalPrice)} ETH
+                        </Button>
+                      </div>
+                    </Card.Footer>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
         </div>
         : (
-          <main style={{ padding: "1rem 0" }}>
-            <h2>No listed assets</h2>
-          </main>
-        )}
-    </div>
+            <main style={{ padding: "1rem 0" }}>
+              <h2>No listed assets</h2>
+            </main>
+          )}
+      </div>
   );
 }
 export default Home
